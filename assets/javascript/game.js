cc0eb1645dc50d12cbabd30a30ec2d8741bfa9de
Christine Nia <christinenia@me.com>
2018-09-19 21:54:38 -0700

    guardians update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,11 +6,19 @@ var winCounter = 0;
 var lossCounter = 0;
 
 
-var crystal1 = Math.floor(Math.random() * 12) + 1;
-var crystal2 = Math.floor(Math.random() * 12) + 1;
-var crystal3 = Math.floor(Math.random() * 12) + 1;
-var crystal4 = Math.floor(Math.random() * 12) + 1;
-var numberOptions = [crystal1, crystal2, crystal3, crystal4];
+var stone1 = Math.floor(Math.random() * 12) + 1;
+var stone2 = Math.floor(Math.random() * 12) + 1;
+var stone3 = Math.floor(Math.random() * 12) + 1;
+var stone4 = Math.floor(Math.random() * 12) + 1;
+var stone5 = Math.floor(Math.random() * 12) + 1;
+var stone6 = Math.floor(Math.random() * 12) + 1;
+var numberOptions = [stone1, stone2, stone3, stone4, stone5, stone6];
+// var stone1Total = 0;
+// var stone2Total = 0;
+// var stone3Total = 0;
+// var stone4Total = 0;
+// var stone5Total = 0;
+// var stone6Total = 0;
 
 
 $("#number-to-guess").text(targetNumber);
@@ -22,35 +30,82 @@ function resetGame() {
   $(".score2").html("Your total score is: " + counter);
 }
 
-for(var i = 0; i < numberOptions.length; i++) {
-  var imageCrystal = $("<img>");
-  imageCrystal.addClass("crystal-image");
-  imageCrystal.attr("src", "assets/images/crystal.jpg");
-  imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-  $("#crystals").append(imageCrystal);
+// function stones(button) {
+// $(button).on("click", function () {
+//   var clickedPic = $(button).attr("alt");
+
+// if(clickedPic === "stone1") {
+//   counter += 1;
+//   stone1Total = stone1 * counter;
+//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+// }
+// if(clickedPic === "stone2") {
+//   counter += 1;
+//   stone2Total = stone2 * counter;
+//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+// }
+// if(clickedPic === "stone3") {
+//   counter += 1;
+//   stone3Total = stone3 * counter;
+//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+// }
+// if(clickedPic === "stone4") {
+//   counter += 1;
+//   stone4Total = stone4 * counter;
+//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+// }
+// if(clickedPic === "stone5") {
+//   counter += 1;
+//   stone5Total = stone5 * counter;
+//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+// }
+// if(clickedPic === "stone6") {
+//   counter += 1;
+//   stone6Total = stone6 * counter;
+//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+// }
+// $(".score2").html("Your total score is: " + counter);
+
+// })
+
+// }
+
+for(var i = 0; i < numberOptions.length;i++) {
+  var imageStone = $("<img>");
+  imageStone.addClass("stones-image");
+  imageStone.attr("src", "assets/images/multiple stones.png");
+  imageStone.attr("data-stonevalue", numberOptions[i]);
+  $("#stones").append(imageStone);
 }
 
-$(".crystal-image").on("click", function() {
-  var crystalValue = ($(this).attr("data-crystalvalue"));
-  crystalValue = parseInt(crystalValue);
-  counter += crystalValue;
+$(".stones-image").on("click", function() {
+  var stoneValue = ($(this).attr("data-stonevalue"));
+  stoneValue = parseInt(stoneValue);
+  counter += stoneValue;
 $(".score2").html("Your total score is: " + counter);
  
 
-  if (counter == targetNumber) {
+  if (counter === targetNumber) {
       winCounter++;
-    alert("You win!");
+    alert("You are one step closer to saving the Universe!");
     $("#wins").text(winCounter);
     resetGame();
   }
   else if (counter >= targetNumber) {
       lossCounter++;
-    alert("You lose!");
+    alert("OH NO, Thanos is one step closer to completing his Infinity Guantlet!");
     $("#losses").text(lossCounter)
     resetGame();
   }
 
+// stones($(".stone1"));
+// stones($(".stone2"));
+// stones($(".stone3"));
+// stones($(".stone4"));
+// stones($(".stone5"));
+// stones($(".stone6"));
+// resetGame();
+// });
 
-});
-
+})
 })
