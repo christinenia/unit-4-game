96bd28d80744b11269d012ba7dbe9db3c58a4c03
Christine Nia <christinenia@me.com>
2018-09-20 17:54:43 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,102 +12,122 @@ var stone3 = Math.floor(Math.random() * 12) + 1;
 var stone4 = Math.floor(Math.random() * 12) + 1;
 var stone5 = Math.floor(Math.random() * 12) + 1;
 var stone6 = Math.floor(Math.random() * 12) + 1;
-var numberOptions = [stone1, stone2, stone3, stone4, stone5, stone6];
 var audio = new Audio("/Users/cnyokoyama/Documents/UCLA/Homework/unit-4-game/Mr Blue Sky.mp3");
-// var stone1Total = 0;
-// var stone2Total = 0;
-// var stone3Total = 0;
-// var stone4Total = 0;
-// var stone5Total = 0;
-// var stone6Total = 0;
+audio.play();
+var stone1Total = 0;
+var stone2Total = 0;
+var stone3Total = 0;
+var stone4Total = 0;
+var stone5Total = 0;
+var stone6Total = 0;
 
 
 $("#number-to-guess").text(targetNumber);
 
+
 function resetGame() {
   targetNumber = Math.floor(Math.random() * ((120 + 1) - 19)) + 19;
   counter = 0;
+  stone1 = Math.floor(Math.random() * 12) + 1;
+  stone2 = Math.floor(Math.random() * 12) + 1;
+  stone3 = Math.floor(Math.random() * 12) + 1;
+  stone4 = Math.floor(Math.random() * 12) + 1;
+  stone5 = Math.floor(Math.random() * 12) + 1;
+  stone6 = Math.floor(Math.random() * 12) + 1;
   $("#number-to-guess").text(targetNumber);
   $(".score2").html("Your total score is: " + counter);
 }
 
-// function stones(button) {
-// $(button).on("click", function () {
-//   var clickedPic = $(button).attr("alt");
+function stones(button) {
+$(button).on("click", function () {
+  var clickedPic = $(button).attr("alt");
 
-// if(clickedPic === "stone1") {
-//   counter += 1;
-//   stone1Total = stone1 * counter;
-//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
-// }
-// if(clickedPic === "stone2") {
-//   counter += 1;
-//   stone2Total = stone2 * counter;
-//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
-// }
-// if(clickedPic === "stone3") {
-//   counter += 1;
-//   stone3Total = stone3 * counter;
-//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
-// }
-// if(clickedPic === "stone4") {
-//   counter += 1;
-//   stone4Total = stone4 * counter;
-//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
-// }
-// if(clickedPic === "stone5") {
-//   counter += 1;
-//   stone5Total = stone5 * counter;
-//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
-// }
-// if(clickedPic === "stone6") {
-//   counter += 1;
-//   stone6Total = stone6 * counter;
-//   var counter = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
-// }
-// $(".score2").html("Your total score is: " + counter);
+if(clickedPic === "stone1") {
+  counter += 1;
+  stone1Total = stone1 * counter;
+  var total = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+}
+if(clickedPic === "stone2") {
+  counter += 1;
+  stone2Total = stone2 * counter;
+  var total = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+}
+if(clickedPic === "stone3") {
+  counter += 1;
+  stone3Total = stone3 * counter;
+  var total = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+}
+if(clickedPic === "stone4") {
+  counter += 1;
+  stone4Total = stone4 * counter;
+  var total = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+}
+if(clickedPic === "stone5") {
+  counter += 1;
+  stone5Total = stone5 * counter;
+  var total = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+}
+if(clickedPic === "stone6") {
+  counter += 1;
+  stone6Total = stone6 * counter;
+  var total = stone1Total + stone2Total + stone3Total + stone4Total + stone5Total + stone6Total;
+}
+$(".score2").html("Your total score is: " + total);
 
-// })
-
-// }
-
-for(var i = 0; i < numberOptions.length;i++) {
-  var imageStone = $("<img>");
-  imageStone.addClass("stones-image");
-  imageStone.attr("src", "assets/images/multiple stones.png");
-  imageStone.attr("data-stonevalue", numberOptions[i]);
-  $("#stones").append(imageStone);
+if (total === targetNumber) {
+  winCounter++;
+alert("You are one step closer to saving the Universe!");
+$("#wins").text(winCounter);
+resetGame();
+}
+else if (total >= targetNumber) {
+  lossCounter++;
+alert("OH NO, Thanos is one step closer to completing his Infinity Guantlet!");
+$("#losses").text(lossCounter)
+resetGame();
 }
 
-$(".stones-image").on("click", function() {
-  audio.play();
-  var stoneValue = ($(this).attr("data-stonevalue"));
-  stoneValue = parseInt(stoneValue);
-  counter += stoneValue;
-$(".score2").html("Your total score is: " + counter);
+})
+
+}
+
+// for(var i = 0; i < numberOptions.length;i++) {
+//   var imageStone = $("<img>");
+//   imageStone.addClass("stones-image");
+//   imageStone.attr("src", "assets/images/multiple stones.png");
+//   imageStone.attr("data-stonevalue", numberOptions[i]);
+//   $("#stones").append(imageStone);
+// }
+
+// $(".stones-image").on("click", function() {
+//   audio.play();
+//   var stoneValue = ($(this).attr("data-stonevalue"));
+//   stoneValue = parseInt(stoneValue);
+//   counter += stoneValue;
+// $(".score2").html("Your total score is: " + counter);
  
 
-  if (counter === targetNumber) {
-      winCounter++;
-    alert("You are one step closer to saving the Universe!");
-    $("#wins").text(winCounter);
-    resetGame();
-  }
-  else if (counter >= targetNumber) {
-      lossCounter++;
-    alert("OH NO, Thanos is one step closer to completing his Infinity Guantlet!");
-    $("#losses").text(lossCounter)
-    resetGame();
-  }
+  // if (counter === targetNumber) {
+  //     winCounter++;
+  //   alert("You are one step closer to saving the Universe!");
+  //   $("#wins").text(winCounter);
+  //   resetGame();
+  // }
+  // else if (counter >= targetNumber) {
+  //     lossCounter++;
+  //   alert("OH NO, Thanos is one step closer to completing his Infinity Guantlet!");
+  //   $("#losses").text(lossCounter)
+  //   resetGame();
+  // }
 
-// stones($(".stone1"));
-// stones($(".stone2"));
-// stones($(".stone3"));
-// stones($(".stone4"));
-// stones($(".stone5"));
-// stones($(".stone6"));
-// resetGame();
-// });
-
-})
+stones($(".stone1"));
+stones($(".stone2"));
+stones($(".stone3"));
+stones($(".stone4"));
+stones($(".stone5"));
+stones($(".stone6"));
+resetGame();
 });
+
+// 
+// });
